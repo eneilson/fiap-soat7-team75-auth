5be6ad8e0c4f46fc0e2e7842373300a4c8356e94
Eneilson Almeida <contato@eneilson.com>
2024-10-01 18:44:59 -0300

    feat(login): cognito integration

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const { CognitoUserPool } = require('amazon-cognito-identity-js');
 
 exports.APP_USER_POOL = new CognitoUserPool({
-  UserPoolId: String(process.env.UserPoolId),
-  ClientId: String(process.env.ClientId),
+  UserPoolId: 'us-east-1_ULx3RiNZI',
+  ClientId: '38797i5cp444p43bskrai4un58',
 });
