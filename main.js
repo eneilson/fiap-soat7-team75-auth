90c593cfd4a523b94edcdc41416426152a4d48f7
Eneilson Almeida <contato@eneilson.com>
2024-10-01 22:32:16 -0300

    feat(login): cognito integration

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const authUser  = require('./src/auth-user');
 const createUser  = require('./src/create-user');
 
-exports.handler = async ({ body, method, ...request }) => {
+exports.handler = async ({ body, httpMethod, ...request }) => {
   const data = typeof body === 'string' ? JSON.parse(body) : body;
 
-  switch (method) {
+  switch (httpMethod) {
     case'POST': 
       return authUser(data);
     case'PUT': 
       return createUser(data);
     default: 
-      return { fail: 'unknown method', request };
+      return { fail: 'unknown method' };
   }
 };
 
